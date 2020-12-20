c1f604593ddbe220303daa0ad180c2573692a565
chanin <pci2676@gmail.com>
2020-12-21 02:30:16 +0900

    feat : 일급 컬렉션 테스트 프로젝트 세팅

---
+++ first-collection/src/main/java/com/javabom/bomjpa/FirstCollectionApplication.java
@@ -0,0 +1,11 @@
+package com.javabom.bomjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCollectionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FirstCollectionApplication.class, args);
+    }
+}
